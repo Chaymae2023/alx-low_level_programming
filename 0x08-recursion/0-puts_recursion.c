9b6722b6438f06e65625542f979497982b7c7814
Chaymae2023 <saimchaymae2022@gmail.com>
2023-04-05 17:44:03 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+     *puts_recursion – prints a string followed by a new line.
+     *@s : pointer to string.
+     *
+     *Return : void.
+     */
+Void _puts_recursion(char *s)
+{
+            If (*s == ‘\0') /* base case */
+          {
+                        _putchar( ‘ \n’ )
+                          return ;
+           }
+           _putchar( *s ) ; 
+           _puts_recursion(s + 1) :             
+  } 
+
